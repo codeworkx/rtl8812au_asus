e5f26f4a65b04c1d698d13854572066138733543
Diederik de Haas <github@cknow.org>
2016-06-24 19:06:15 +0200

    Add support for kernels >= 4.7.
    This conditionally redefines 'enum ieee80211_band' and 'IEEE80211_NUM_BANDS'
    with the pre-processor based on kernel version. Thanks to yorkedork for
    providing the patch.
    References: https://github.com/Grawp/rtl8812au_rtl8821au/pull/19

--- include/rtw_wifi_regd.h
+++ include/rtw_wifi_regd.h
@@ -24,5 +24,11 @@ int rtw_regd_init(_adapter *padapter,
 		struct regulatory_request *request));
 void rtw_reg_notifier(struct wiphy *wiphy, struct regulatory_request *request);
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 7, 0))
+#define ieee80211_band nl80211_band
+#define IEEE80211_BAND_2GHZ NL80211_BAND_2GHZ
+#define IEEE80211_BAND_5GHZ NL80211_BAND_5GHZ
+#define IEEE80211_NUM_BANDS NUM_NL80211_BANDS
+#endif
 
 #endif
